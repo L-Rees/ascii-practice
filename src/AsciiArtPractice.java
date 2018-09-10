80f41ed1062e2eeabde3bcbfb2eb470418fc099b
Lisa Rees <kortirion@gmail.com>
2018-09-10 19:09:13 -0400

    add more cats

--- src/AsciiArtPractice.java
+++ src/AsciiArtPractice.java
@@ -29,36 +29,128 @@ public class AsciiArtPractice {
 
 		//awesome
 		String catBike;
-		catBike = "               ________________ \n" +
-	"              | You're not my  |_____    __ \n" +
-	"              | friend anymore |     |__|  |_________ \n" +
-	"   /\\         |________________| Good|::|  |        / \n" +
-	" /\\||/\\       |                \\.____|::|__| bye! <	\n" +
-	"( o_o  )_     |                      \\::/  \\._______\\ \n" +
-	" (u--u   \\_)  | \n" +
-	"  (||___   )==\\ \n" +
-	",dP\"/b/=( /P\"/b\\ \n" +
-	"|8 || 8\\=== || 8 \n" +
-	"`b,  ,P  `b,  ,P \n" +
-	"  \"\"\"`     \"\"\"` \n";
+		catBike = "                 ________________ \n" +
+	"                | You're NOT my  |_____    __ \n" +
+	"                | friend anymore |     |__|  |_________ \n" +
+	"     /\\         |________________| Good|::|  |        / \n" +
+	"   /\\||/\\       |                \\.____|::|__| BYE  <	\n" +
+	"  ( o_o  )_     |                      \\::/  \\._______\\ \n" +
+	"   (u--u   \\_)  | \n" +
+	"    (||___   )==\\ \n" +
+	"  ,dP\"/b/=( /P\"/b\\ \n" +
+	"  |8 || 8\\=== || 8 \n" +
+	"  `b,  ,P  `b,  ,P \n" +
+	"    \"\"\"`     \"\"\"` \n";
 		
 		System.out.println(catBike);
 				
 		String angryCat;
-		angryCat ="    /\\   \n" +
-				  "  /\\||/\\ \n" +
-				  " (  \\ /  ) \n" +
-				  " /  o o  \\ \n" +
-				  " \\__~~~__/ grr! \n" +
-				  "   /   \\ \n" +
-				  "  / ___ \\ \n" +
-				  "  \\/___\\/ \n";
+		angryCat ="      /\\   \n" +
+				  "    /\\|| /\\ \n" +
+				  "   (  \\ /  ) \n" +
+				  "   /  o,o  \\ \n" +
+				  "   \\__~~~__/ grr! \n" +
+				  "     /   \\  /\n" +
+				  "    / ___ \\/ \n" +
+				  "    \\/___\\/ \n";
 
 		System.out.println(angryCat);
 		
+		String excitedCat;
+		excitedCat = "      /\\   \n" +
+				  "    /\\|| /\\ \n" +
+				  "   (  ^ ^  ) \n" +
+				  "   /   }o{ \\ \n" +
+				  "   \\__   __/ ooh! \n" +
+				  "     /   \\  /\n" +
+				  "    / ___ \\/ \n" +
+				  "    \\/___\\/ \n";
+		System.out.println(excitedCat);
+		
+		String boredCat;
+		boredCat = "      /\\   \n" +
+				  "    /\\|| /\\ \n" +
+				  "   ( <o o> ) \n" +
+				  "   /  >o<  \\ \n" +
+				  "   \\__ ~ __/ sigh! \n" +
+				  "     /   \\  /\n" +
+				  "    / ___ \\/ \n" +
+				  "    \\/___\\/ \n";
+		
+		System.out.println(boredCat);
+		
+		String happyCat;
+		happyCat = "      /\\   \n" +
+				  "    /\\|| /\\ \n" +
+				  "   (  . .  ) \n" +
+				  "   /  >.<  \\ \n" +
+				  "   \\__\\_/__/ \n" + 
+				  "     /   \\  / \n" +
+				  "    / ___ \\/ \n" +
+				  "    \\/___\\/ \n";
+		
+		System.out.println(happyCat);
+		
+		String prettyCat;
+		prettyCat = "      /\\   \n" +
+				  " *  /\\|| /\\  *  \n" +
+				  "   (  _  _ ) \n" +
+				  "   /  ___  \\   * \n" +
+				  "   \\__\\_/__/ \n" +
+				  "  *  /   \\  /\n" +
+				  "    / ___ \\/   *\n" +
+				  " *  \\/___\\/  *\n";
+		
+		System.out.println(prettyCat);
+		
+		String templateCat;
+		templateCat = "      /\\   \n" +
+				  "    /\\|| /\\ \n" +
+				  "   (       ) \n" +
+				  "   /       \\ \n" +
+				  "   \\__   __/ mew! \n" +
+				  "     /   \\  /\n" +
+				  "    / ___ \\/ \n" +
+				  "    \\/___\\/ \n";
+		
+		System.out.println(templateCat);
+		
+		String offendedCat;
+		offendedCat = "      /\\   \n" +
+				  "    /\\|| /\\ \n" +
+				  "   (       ) \n" +
+				  "   /  >.<  \\ \n" +
+				  "   \\__ 0 __/ as if! \n" +
+				  "     /   \\  /\n" +
+				  "    / ___ \\/ \n" +
+				  "    \\/___\\/ \n";
+		
+		System.out.println(offendedCat);
 		
 		
+		String reallyHungryCat;
+		reallyHungryCat = "      /\\   \n" +
+				  "    /\\|| /\\ \n" +
+				  "   (  -  - ) \n" +
+				  "   /  >.<  \\ \n" +
+				  "   \\__ ~~__/ tummy grumbles! \n" +
+				  "     /   \\  /\n" +
+				  "    / ___ \\/ \n" +
+				  "    \\/___\\/ \n";
 		
+		System.out.println(reallyHungryCat);
+		
+		String hungryCat;
+		hungryCat = "      /\\   \n" +
+				  "    /\\|| /\\ \n" +
+				  "   ( <o o> ) \n" +
+				  "   /  >.<  \\ \n" +
+				  "   \\__ O __/ hungry! \n" +
+				  "     /   \\  /\n" +
+				  "    / ___ \\/ \n" +
+				  "    \\/___\\/ \n";
+		
+		System.out.println(hungryCat);
 		
 		
 		
