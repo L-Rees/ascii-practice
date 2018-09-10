e60e9b5d54f984ee97a40cfc9f3ddb35564720ed
Lisa Rees <kortirion@gmail.com>
2018-09-10 14:48:32 -0400

    fist commit

---
+++ src/AsciiArtPractice.java
@@ -0,0 +1,57 @@
+
+public class AsciiArtPractice {
+
+	public static void main(String[] args) {
+
+		String unikittyName;
+		unikittyName = 
+				 " _   _      _ _  ___ _   _        \n" +
+				 "| | | |_ _ (_) |/ (_) |_| |_ _  _ \n"+
+				 "| |_| | ' \\| | ' <| |  _|  _| || |\n"+
+				 " \\___/|_||_|_|_|\\_\\_|\\__|\\__|\\_, | \n" +
+				 "                              |__/  \n";
+
+		System.out.println(unikittyName);
+		
+		//this one
+		String unikittyName2;
+		unikittyName2 = 
+				 "  _    _       _ _    _ _   _         \n" +
+				 " | |  | |     (_) |  (_) | | |        \n"+
+				 " | |  | |_ __  _| | ___| |_| |_ _   _ \n" +
+				 " | |  | | '_ \\| | |/ / | __| __| | | |\n" +
+				 " | |__| | | | | |   <| | |_| |_| |_| |\n" +
+				 "  \\____/|_| |_|_|_|\\_\\_|\\__|\\__|\\__, |\n" +
+				 "                                 __/ |\n" +
+				 "                                |___/ \n";
+		
+		System.out.println(unikittyName2);
+
+		//awesome
+		String catBike;
+		catBike = "               ________________ \n" +
+	"              | You're not my  |_____    __ \n" +
+	"              | friend anymore |     |__|  |_________ \n" +
+	"   /\\         |________________| Good|::|  |        / \n" +
+	" /\\||/\\       |                \\.____|::|__| bye! <	\n" +
+	"( o_o  )_     |                      \\::/  \\._______\\ \n" +
+	" (u--u   \\_)  | \n" +
+	"  (||___   )==\\ \n" +
+	",dP\"/b/=( /P\"/b\\ \n" +
+	"|8 || 8\\=== || 8 \n" +
+	"`b,  ,P  `b,  ,P \n" +
+	"  \"\"\"`     \"\"\"` \n";
+		
+		System.out.println(catBike);
+				
+				
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
